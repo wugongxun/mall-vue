721c5411bea40f4f44ddf1d8a4e6c64a50e01a45
wugongxun <2297665453@qq.com>
2023-03-08 18:15:11 +0800

    2023.3.8

--- src/views/modules/product/brand-add-or-update.vue
+++ src/views/modules/product/brand-add-or-update.vue
@@ -163,28 +163,17 @@ export default {
             })
         },
         async beforeAvatarUpload (file) {
-            const isJPG = file.type === 'image/jpeg';
-            const isLt2M = file.size / 1024 / 1024 < 2;
-
-            if (!isJPG) {
-                this.$message.error('上传图片只能是 JPG 格式!');
-            }
-            if (!isLt2M) {
-                this.$message.error('上传图片大小不能超过 2MB!');
-            }
-            if (isJPG && isLt2M) {
-                await this.getSignature().then(res => {
-                    let sign = this.$store.state.brand.signature
-                    this.signature = {
-                        policy: sign.policy,
-                        signature: sign.signature,
-                        key: sign.dir + uuid() + "_${filename}",
-                        ossaccessKeyId: sign.accessId,
-                        dir: sign.dir,
-                        host: sign.host
-                    }
-                });
-            }
+            await this.getSignature().then(res => {
+                let sign = this.$store.state.brand.signature
+                this.signature = {
+                    policy: sign.policy,
+                    signature: sign.signature,
+                    key: sign.dir + uuid() + "_${filename}",
+                    ossaccessKeyId: sign.accessId,
+                    dir: sign.dir,
+                    host: sign.host
+                }
+            });
         },
         handleAvatarSuccess(res, file) {
             this.dataForm.logo = this.signature.host + '/' + this.signature.key.replace("${filename}",file.name);
